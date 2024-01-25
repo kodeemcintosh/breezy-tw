8b6fb726332f84bc924fd643fb8160dc937dd6a0
kodeemcintosh <19210856+kodeemcintosh@users.noreply.github.com>
2024-01-25 00:45:29 -0500

    fixed export

--- lib/main.ts
+++ lib/main.ts
@@ -4,8 +4,7 @@ import './index.css';
 import { Badge } from './components/labels/badge/Badge';
 import { LabelWrapper } from './components/labels/label-wrapper/LabelWrapper';
 import { Input } from './components/input/Input';
-import { SearchInput } from './components/inputs/search-input/SearchInput';
 import { Button } from './components/button/Button';
 
 
-export { Badge, Input, Button, LabelWrapper, SearchInput };
+export { Badge, Input, Button, LabelWrapper };
